f3dcb9364c10d848c1ea78199e97077631f60d88
MikaboshiDev <21011073@itsoeh.edu.mx>
2024-03-27 11:42:54 -0600

    Añadir .map a .gitignore

---
+++ lib/types/functions/animelist.d.ts
@@ -0,0 +1,6 @@
+export declare function animeRandom(): Promise<any>;
+export declare function mangaRandom(): Promise<any>;
+export declare function mangaSearch(id: number): Promise<any>;
+export declare function animeSearch(id: number): Promise<any>;
+export declare function animeRecommended(): Promise<any>;
+export declare function mangaRecommended(): Promise<any>;
